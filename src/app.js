b01cea5e620469e2686c4545634f164f3ba7f3d1
0keeffee <137979040+0keeffee@users.noreply.github.com>
2023-09-02 07:06:30 +0000

    generate a new excuse every time you press the button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", function() {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  function generateExcuse() {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+    document.getElementById("excuse").textContent = excuse;
+  }
+
+  document
+    .getElementById("excuseButton")
+    .addEventListener("click", generateExcuse);
+});
